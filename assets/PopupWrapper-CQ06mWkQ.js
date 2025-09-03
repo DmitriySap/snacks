65cb2f9c0745aeaf4e9073b48aac17ce99d37015
Dmitriy <saprykindima@bk.ru>
2025-09-03 17:03:46 +0300

    Updates

--- assets/PopupWrapper-CQ06mWkQ.js
+++ assets/PopupWrapper-CQ06mWkQ.js
@@ -1,4 +1,4 @@
-import{c as l,e as r,j as e}from"./index-rq1iMH2i.js";/**
+import{c as l,e as r,j as e}from"./index-CbPCQZDh.js";/**
  * @license lucide-react v0.542.0 - ISC
  *
  * This source code is licensed under the ISC license.
